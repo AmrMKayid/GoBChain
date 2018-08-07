44cc291e6c4bbcd82d0ff8ba4a3adfe1a5962385
Amr M. Kayid <amrkayid2027@gmail.com>
2018-08-07 18:04:07 +0200

    Modify blockchain for Persistence

--- block.go
+++ block.go
@@ -33,7 +33,7 @@ func (b *Block) Serialize() []byte {
 	var result bytes.Buffer
 	encoder := gob.NewEncoder(&result)
 
-	_ = encoder.Encode(b)
+	err := encoder.Encode(b)
 
 	return result.Bytes()
 }
@@ -42,7 +42,7 @@ func DeserializeBlock(d []byte) *Block {
 	var block Block
 
 	decoder := gob.NewDecoder(bytes.NewReader(d))
-	_ := decoder.Decode(&block)
+	err := decoder.Decode(&block)
 
 	return &block
 }
